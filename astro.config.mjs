e244f4f8493569c8b526d4ec91123d227daf7522
artem <artem.kan@icons8.com>
2025-10-31 14:47:11 +0200

    fix seo 

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://proshare.dev',
   integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: 'en',
